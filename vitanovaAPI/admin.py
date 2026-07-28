52636ad095f996b20f2540f841aae7bc77d01162
Ayebea3 <ayebeaayeh3@gmail.com>
2026-07-28 20:45:28 +0100

    registered models

--- vitanovaAPI/admin.py
+++ vitanovaAPI/admin.py
@@ -5,6 +5,10 @@ from .models.patients_models import Patient
 from .models.location_models import Location
 from .models.notification_models import Notification
 from .models.organisation_model  import Organization
+from .models.blood_donation_model import BloodDonation 
+from .models.blood_request_model import BloodRequest 
+from .models.inventory_model import Inventory 
+from .models.blood_models import Blood  
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Role)
@@ -13,3 +17,7 @@ admin.site.register(Patient)
 admin.site.register(Location)
 admin.site.register(Notification)
 admin.site.register(Organization)
+admin.site.register(BloodDonation)
+admin.site.register(BloodRequest)
+admin.site.register(Inventory)
+admin.site.register(Blood)
