9b43134b5479ab7e5f24b181e3bb80932fa189c2
Engineer Sam <sammyyawsnana1@gmail.com>
2026-08-05 01:58:39 +0000

    removed exposed secret key in settings

--- vitanovaBack/settings.py
+++ vitanovaBack/settings.py
@@ -19,10 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@$j+0t^1wwynwc_$&o2#==-0*v%(nkxkl!$y2w=hl(cnwi!ip^'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 
 
 
